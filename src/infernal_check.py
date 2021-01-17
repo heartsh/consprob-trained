088e6cec411907ec097a4b92ae4ae0aa1bcdb251
heartsh <heartsh@heartsh.io>
2021-01-18 02:38:18 +0900

    Upload compressed files

--- src/infernal_check.py
+++ src/infernal_check.py
@@ -28,25 +28,22 @@ def main():
   temp_dir_path = "/tmp/infernal_check_%s" % datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d_%H:%M:%S')
   if not os.path.isdir(temp_dir_path):
     os.mkdir(temp_dir_path)
-  if False:
-    for sa_file in os.listdir(test_ref_sa_dir_path):
-      if not sa_file.endswith(".sth"):
-        continue
-      (sa_file_name, extension) = os.path.splitext(sa_file)
-      sa_file_path = os.path.join(test_ref_sa_dir_path, sa_file)
-      infernal_output_file_path = os.path.join(test_ref_sa_dir_path, sa_file_name + "_infernal.dat")
-      infernal_build_command = "cmbuild -F " + infernal_output_file_path + " " + sa_file_path
-      utils.run_command(infernal_build_command)
-      infernal_calib_command = "cmcalibrate " + infernal_output_file_path
-      utils.run_command(infernal_calib_command)
+  for sa_file in os.listdir(test_ref_sa_dir_path):
+    if not sa_file.endswith(".sth"):
+      continue
+    (sa_file_name, extension) = os.path.splitext(sa_file)
+    sa_file_path = os.path.join(test_ref_sa_dir_path, sa_file)
+    infernal_output_file_path = os.path.join(test_ref_sa_dir_path, sa_file_name + "_infernal.dat")
+    infernal_build_command = "cmbuild -F " + infernal_output_file_path + " " + sa_file_path
+    utils.run_command(infernal_build_command)
+    infernal_calib_command = "cmcalibrate " + infernal_output_file_path
+    utils.run_command(infernal_calib_command)
   temp_seq_file_path = os.path.join(temp_dir_path, "temp.fa")
   temp_seq_file = open(temp_seq_file_path, "w")
   for seq_file in os.listdir(train_data_dir_path):
     if not seq_file.endswith(".fa"):
       continue
     seq_file_path = os.path.join(train_data_dir_path, seq_file)
-    # temp_seq_file_path = os.path.join(temp_dir_path, seq_file)
-    # temp_seq_file = open(temp_seq_file_path, "w")
     for j, rec in enumerate(SeqIO.parse(seq_file_path, "fasta")):
       if j >= 2:
         break
@@ -62,10 +59,8 @@ def main():
     infernal_black_list_file_path = os.path.join(infernal_black_list_dir_path, sa_file)
     infernal_search_command = "cmsearch -E 0.001 " + sa_file_path + " " + temp_seq_file_path
     (output, _, _) = utils.run_command(infernal_search_command)
-    # print(str(output))
     if "No hits detected" not in str(output):
       shutil.copyfile(sa_file_path, infernal_black_list_file_path)
-      print(sa_file_path)
 
   shutil.rmtree(temp_dir_path)
 
