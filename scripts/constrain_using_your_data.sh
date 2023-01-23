208963fc10819d8d07f1b34aeba450a50a432340
heartsh <heartsh@heartsh.io>
2023-01-24 07:42:31 +0900

    Add script

---
+++ scripts/constrain_using_your_data.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env sh
+
+TRAIN_DATA="$1"
+shift
+TRAIN_LOG="$1"
+shift
+
+RUSTFLAGS='--emit asm -C target-feature=+avx -C target-feature=+ssse3 -C target-feature=+mmx' cargo install consprob-trained
+constrain -i $TRAIN_DATA -o $TRAIN_LOG
+sed -E -i "s/^(consprob-trained=)\"[0-9]+\.[0-9]+\"$/\1={path = \"\.\/consprob-trained\"}/" ../../Cargo.toml
+RUSTFLAGS='--emit asm -C target-feature=+avx -C target-feature=+ssse3 -C target-feature=+mmx' cargo install --path ../.. -f
