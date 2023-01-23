6034a8bffb5105d22505441e58a835ceb4ce06d2
heartsh <heartsh@heartsh.io>
2023-01-24 08:03:50 +0900

    Fix script

--- scripts/constrain_using_your_data.sh
+++ scripts/constrain_using_your_data.sh
@@ -5,7 +5,7 @@ shift
 TRAIN_LOG="$1"
 shift
 
-RUSTFLAGS='--emit asm -C target-feature=+avx -C target-feature=+ssse3 -C target-feature=+mmx' cargo install consprob-trained
+RUSTFLAGS='--emit asm -C target-feature=+avx -C target-feature=+ssse3 -C target-feature=+mmx' cargo install consprob-trained -f
 constrain -i $TRAIN_DATA -o $TRAIN_LOG
-sed -E -i "s/^(consprob-trained=)\"[0-9]+\.[0-9]+\"$/\1={path = \"\.\/consprob-trained\"}/" ../../Cargo.toml
+sed -E -i "s/^(consprob-trained = )\"[0-9]+\.[0-9]+\"$/\1{path = \"\.\/consprob-trained\"}/" ../../Cargo.toml
 RUSTFLAGS='--emit asm -C target-feature=+avx -C target-feature=+ssse3 -C target-feature=+mmx' cargo install --path ../.. -f
